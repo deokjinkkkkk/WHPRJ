dff4c92a6f667d6f556c5cff456960fcd3c73eac
seodeokjin <antan3957@naver.com>
2023-02-17 17:42:58 +0900

    test

---
+++ whprj/src/main/java/co/micol/whprj/WhprjApplication.java
@@ -0,0 +1,13 @@
+package co.micol.whprj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhprjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhprjApplication.class, args);
+	}
+
+}
---
+++ whprj/src/test/java/co/micol/whprj/WhprjApplicationTests.java
@@ -0,0 +1,13 @@
+package co.micol.whprj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhprjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
